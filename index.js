6855ea827d8533b3dd7e5bb64f788878caaf4190
Matt Ozrelic <2729163+mozrelic@users.noreply.github.com>
2023-01-27 23:01:19 -0800

    set up initial code to display weather info

--- index.js
+++ index.js
@@ -1,99 +1,101 @@
-("use strict");
+('use strict');
 
-import "./index.scss";
-import clockView from "./clockView.js";
-import { getGeo } from "./src/js/weather.js";
+import './index.scss';
+import clockView from './clockView.js';
+import { getWeather, renderWeather } from './src/js/weather.js';
 
-getGeo();
+const weatherData = await getWeather();
+console.log(weatherData);
+renderWeather(weatherData);
 
-const target = document.querySelector(".clock-container");
-const hourEl = document.querySelector(".hour");
-const minutesEl = document.querySelector(".minutes");
-const secondsEl = document.querySelector(".seconds");
-const antePostEl = document.querySelector(".am-pm");
+const target = document.querySelector('.clock-container');
+const hourEl = document.querySelector('.hour');
+const minutesEl = document.querySelector('.minutes');
+const secondsEl = document.querySelector('.seconds');
+const antePostEl = document.querySelector('.am-pm');
 
-const dayEl = document.querySelector(".day");
-const monthEl = document.querySelector(".month");
-const yearEl = document.querySelector(".year");
+const dayEl = document.querySelector('.day');
+const monthEl = document.querySelector('.month');
+const yearEl = document.querySelector('.year');
 
 const setDigits = (time) => {
-    hourEl.firstElementChild.setAttribute("class", `num-${time.hh1}`);
-    hourEl.lastElementChild.setAttribute("class", `num-${time.hh2}`);
-    minutesEl.firstElementChild.setAttribute("class", `num-${time.mm1}`);
-    minutesEl.lastElementChild.setAttribute("class", `num-${time.mm2}`);
-    secondsEl.firstElementChild.setAttribute("class", `num-${time.ss1}`);
-    secondsEl.lastElementChild.setAttribute("class", `num-${time.ss2}`);
+    hourEl.firstElementChild.setAttribute('class', `num-${time.hh1}`);
+    hourEl.lastElementChild.setAttribute('class', `num-${time.hh2}`);
+    minutesEl.firstElementChild.setAttribute('class', `num-${time.mm1}`);
+    minutesEl.lastElementChild.setAttribute('class', `num-${time.mm2}`);
+    secondsEl.firstElementChild.setAttribute('class', `num-${time.ss1}`);
+    secondsEl.lastElementChild.setAttribute('class', `num-${time.ss2}`);
 };
 
 const setAntePost = (time) => {
     // get children elements of am-pm parent container
     const { 0: amEl, 1: pmEl } = antePostEl.children;
 
-    const elClass = time.antePost === "am" ? "its-am" : "its-pm";
+    const elClass = time.antePost === 'am' ? 'its-am' : 'its-pm';
 
     // check if both elements have their active class
     const resetLetters =
-        amEl.classList.contains("its-am") && pmEl.classList.contains("its-pm");
+        amEl.classList.contains('its-am') && pmEl.classList.contains('its-pm');
 
     // if both do, remove active class from both
     if (resetLetters) {
-        amEl.classList.remove("its-am");
-        pmEl.classList.remove("its-pm");
+        amEl.classList.remove('its-am');
+        pmEl.classList.remove('its-pm');
     }
 
     // set active class
-    if (time.antePost === "am") amEl.setAttribute("class", `${elClass} `);
+    if (time.antePost === 'am') amEl.setAttribute('class', `${elClass} `);
 
-    if (time.antePost === "pm") pmEl.setAttribute("class", `${elClass}`);
+    if (time.antePost === 'pm') pmEl.setAttribute('class', `${elClass}`);
 };
 
 const setDay = (time) => {
     const { 0: num1, 1: num2 } = dayEl.children;
 
-    num1.setAttribute("class", `num-${time.da1}`);
-    num2.setAttribute("class", `num-${time.da2}`);
+    num1.setAttribute('class', `num-${time.da1}`);
+    num2.setAttribute('class', `num-${time.da2}`);
 };
 
 const setMonth = (time) => {
     const { 0: num1, 1: num2 } = monthEl.children;
 
-    num1.setAttribute("class", `num-${time.mo1}`);
-    num2.setAttribute("class", `num-${time.mo2}`);
+    num1.setAttribute('class', `num-${time.mo1}`);
+    num2.setAttribute('class', `num-${time.mo2}`);
 };
 
 const setYear = (time) => {
     const { 0: num1, 1: num2 } = yearEl.children;
 
-    num1.setAttribute("class", `num-${time.ye1}`);
-    num2.setAttribute("class", `num-${time.ye2}`);
+    num1.setAttribute('class', `num-${time.ye1}`);
+    num2.setAttribute('class', `num-${time.ye2}`);
 };
 
 const getCurrentTime = () => {
     const rawTime = new Date();
 
-    const formatter = new Intl.DateTimeFormat("en-us", {
-        year: "numeric",
-        month: "numeric",
-        day: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
+    const formatter = new Intl.DateTimeFormat('en-us', {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
         hour12: true,
     });
 
     const filteredTime = formatter
         .formatToParts(rawTime)
-        .filter((row) => row["type"] !== "literal");
+        .filter((row) => row['type'] !== 'literal');
 
     // this takes the filtered time object and gets the last two digits from each value string, so i can then save them into a new
     // object with a specific key name. I have done it this way so I can use the final key value as a template literal to render
     // the correct css class
     const tempTimeObj = {};
     filteredTime.forEach(function(item) {
-        let tempTimeValue = item["value"].padStart(2, "0").split("").slice(-2);
+        let tempTimeValue = item['value'].padStart(2, '0').split('').slice(-2);
 
         tempTimeValue.forEach((value, i) => {
-            tempTimeObj[`${item["type"]}${i + 1}`] = value;
+            tempTimeObj[`${item['type']}${i + 1}`] = value;
         });
     });
 
--- src/js/weather.js
+++ src/js/weather.js
@@ -1,16 +1,62 @@
-import axios from "axios";
+import axios from 'axios';
 
-const zipCode = "97756";
-const apiKey = "ef9f7861750cc66b5688bdfad901efd4";
+const zipCode = '97756';
+const apiKey = 'ef9f7861750cc66b5688bdfad901efd4';
+
 const BASE_URL_GEO = `http://api.openweathermap.org/geo/1.0/zip?zip=${zipCode}&appid=${apiKey}`;
+// const BASE_URL_WEATHER = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-export async function getGeo() {
-  console.log(BASE_URL_GEO);
+async function getGeo() {
   try {
     const response = await axios.get(BASE_URL_GEO);
-    const { lat, lon } = response.data;
-    console.log(lat, lon);
+    const data = response.data;
+
+    return data;
   } catch (error) {
     console.error(error);
   }
+}
+
+export async function getWeather() {
+  try {
+    const locationData = await getGeo();
+    const { lat, lon } = locationData;
+
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&exclude=hourly,daily&units=imperial&appid=${apiKey}`
+    );
+    const { data } = response;
+
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export function renderWeather(data) {
+  const target = document.querySelector('.weather-container');
+  const cleanData = {
+    name: data.name,
+    description: data.weather[0].description,
+    iconId: data.weather[0].id,
+    icon: data.weather[0].icon,
+    temp: data.main.temp,
+  };
+
+  const dayOrNight = cleanData.icon.at(-1);
+
+  const weatherMarkup = `
+  <div class="location-title">${cleanData.name}</div>
+    <div class="icon">
+    <i class="wi wi-owm-${dayOrNight === 'n' ? 'night' : 'day'}-${cleanData.iconId
+    }"></i>
+    </div>
+    <div class="description">
+      ${cleanData.description}
+    </div>
+  <div class="temp"> <i class="wi wi-fahrenheit"></i>${cleanData.temp}</div >
+    `;
+
+  target.insertAdjacentHTML('afterbegin', weatherMarkup);
+  // console.log(cleanData);
 }
