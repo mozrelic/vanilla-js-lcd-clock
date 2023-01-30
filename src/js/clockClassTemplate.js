c3934eb6dff6a096c59ade77acea61455ad45e6d
Matt Ozrelic <2729163+mozrelic@users.noreply.github.com>
2023-01-29 23:20:32 -0800

    added a parent class for clock modules

---
+++ src/js/clockClassTemplate.js
@@ -0,0 +1,13 @@
+export class ClockClassTemplate {
+  // timeInterval;
+  // target;
+  // options;
+  defaultOptions = {
+    target: '.clock-container',
+    activeColor: 'undefined',
+    inactiveColor: 'undefined',
+    clockMeta: true,
+    hour12: false,
+  };
+}
+// export default new ClockClassTemplate();
