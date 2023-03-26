c7901d109cf1f710f03559afcd3a8fbf4a54241c
Matt Ozrelic <2729163+mozrelic@users.noreply.github.com>
2023-03-26 02:02:36 -0700

    added open and close function to settings modal. Got settings button animation working. Removed unused file.

--- src/js/helpers/modal.js
+++ src/js/helpers/modal.js
@@ -1,11 +1,17 @@
 export function modalHandler() {
-  const modalContainer = document.querySelector('.modal-container');
-  const modalTrigger = document.querySelector('.modal-trigger button');
-  console.log(modalTrigger);
-  modalTrigger.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (!e.target) return;
-    modalContainer.classList.remove('hidden');
-    modalContainer.classList.add('active');
-  });
+    const modalContainer = document.querySelector('.modal-container');
+    const modalTrigger = document.querySelector('.modal-trigger');
+    const buttonTrigger = document.querySelector('.modal-trigger button');
+    const closeModal = document.querySelector('.close-modal');
+    console.log(modalTrigger);
+    modalTrigger.addEventListener('click', (e) => {
+        if (!e.target) return;
+        modalContainer.classList.remove('hidden');
+        modalContainer.classList.add('active');
+    });
+
+    closeModal.addEventListener('click', (e) => {
+        modalContainer.classList.remove('active');
+        modalContainer.classList.add('hidden');
+    });
 }
