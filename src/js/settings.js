8f031c3c445b433ef6799c4602be23c2654b78e8
Matt Ozrelic <2729163+mozrelic@users.noreply.github.com>
2023-03-30 13:45:22 -0700

    commit before transitioning all state in app over to a modular state

---
+++ src/js/settings.js
@@ -0,0 +1,15 @@
+let currentState = {};
+
+let lastState = {};
+
+let defaultState = {
+    target: '.clock-container',
+    activeColor: '#c9d1d9',
+    inactiveColor: '#262626',
+    clockMeta: true,
+    hour12: false,
+    zipcode: '97756',
+    apiKey: '',
+};
+
+export { currentState, lastState, defaultState };
