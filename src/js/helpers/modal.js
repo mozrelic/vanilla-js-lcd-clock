1cbba5edcfd93a7062f385fcb5e4803c5514c459
Matt Ozrelic <2729163+mozrelic@users.noreply.github.com>
2023-07-21 14:28:57 -0700

    fixing more html validation errors and furthur improving accessibility

--- src/js/helpers/modal.js
+++ src/js/helpers/modal.js
@@ -4,41 +4,37 @@ export function modalHandler() {
     const modalTrigger = document.querySelector('.modal-trigger');
     const closeModal = document.querySelector('.close-modal');
 
-    function closeOrOpenModal(action) {
-        if (action === 'open') {
-            modalContainer.classList.remove('hidden');
-            modalContainer.classList.add('active');
+    function setModalState(target, state, callback) {
+        target.classList.toggle('active', state);
+        if (typeof state !== 'undefined') {
+            state = !state;
+            target.setAttribute('aria-hidden', state);
         }
-
-        if (action === 'close') {
-            modalContainer.classList.remove('active');
-            modalContainer.classList.add('hidden');
-        }
-    }
-
-    function escOnParentClick(e) {
-        if (e.target.classList.contains('modal-container')) {
-            closeOrOpenModal('close');
-            console.log(e.target);
-        }
+        callback;
     }
 
     function escClose(e) {
         if (!e.keyCode || e.keyCode === 27) {
-            closeOrOpenModal('close');
+            setModalState(modalContainer, false);
         }
     }
 
     modalTrigger.addEventListener('click', (e) => {
         if (!e.target) return;
-        closeOrOpenModal('open');
-        slideIn('.column');
+        setModalState(modalContainer, true, slideIn('.column'));
     });
 
-    closeModal.addEventListener('click', () => {
-        closeOrOpenModal('close');
+    closeModal.addEventListener('click', (e) => {
+        if (!e.target) return;
+        setModalState(modalContainer, false);
     });
 
-    modalContainer.addEventListener('click', escOnParentClick);
+    modalContainer.addEventListener('click', (e) => {
+        if (!e.target) return;
+        if (e.target.classList.contains('modal-container')) {
+            setModalState(modalContainer, false);
+        }
+    });
+
     document.addEventListener('keydown', escClose);
 }
