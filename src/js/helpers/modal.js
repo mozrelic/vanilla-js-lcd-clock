649c9e6ab230d5a430aa81a96d216e728b48b1cc
Matt Ozrelic <2729163+mozrelic@users.noreply.github.com>
2023-07-20 18:36:06 -0700

    fixed validation errors and accessibility errors

--- src/js/helpers/modal.js
+++ src/js/helpers/modal.js
@@ -4,15 +4,41 @@ export function modalHandler() {
     const modalTrigger = document.querySelector('.modal-trigger');
     const closeModal = document.querySelector('.close-modal');
 
+    function closeOrOpenModal(action) {
+        if (action === 'open') {
+            modalContainer.classList.remove('hidden');
+            modalContainer.classList.add('active');
+        }
+
+        if (action === 'close') {
+            modalContainer.classList.remove('active');
+            modalContainer.classList.add('hidden');
+        }
+    }
+
+    function escOnParentClick(e) {
+        if (e.target.classList.contains('modal-container')) {
+            closeOrOpenModal('close');
+            console.log(e.target);
+        }
+    }
+
+    function escClose(e) {
+        if (!e.keyCode || e.keyCode === 27) {
+            closeOrOpenModal('close');
+        }
+    }
+
     modalTrigger.addEventListener('click', (e) => {
         if (!e.target) return;
-        modalContainer.classList.remove('hidden');
-        modalContainer.classList.add('active');
+        closeOrOpenModal('open');
         slideIn('.column');
     });
 
     closeModal.addEventListener('click', () => {
-        modalContainer.classList.remove('active');
-        modalContainer.classList.add('hidden');
+        closeOrOpenModal('close');
     });
+
+    modalContainer.addEventListener('click', escOnParentClick);
+    document.addEventListener('keydown', escClose);
 }
