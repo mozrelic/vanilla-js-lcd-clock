4eafa499d30b8e84684d096422059e9b1c7e1c5f
Matt Ozrelic <2729163+mozrelic@users.noreply.github.com>
2024-08-11 18:04:53 -0700

    something about my animateTransition helper class is broken. I think it's because it's a singleton. Going to refactor.

--- src/js/helpers/Animation.js
+++ src/js/helpers/Animation.js
@@ -1,67 +1,81 @@
 class AnimateTransition {
-    target;
-    animationType;
-    time = 50;
-    delay = 50;
+  target;
+  animationType;
+  time = 50;
+  delay = 50;
 
-    startAnimation(target, animationType = 'slideIn') {
-        if (!target) return;
+  // constructor(target, animationType = 'slideIn'){
+  //     this.target = document.querySelectorAll(target);
+  //     this.animationType = animationType;
+  // }
 
-        this.target = document.querySelectorAll(target);
-        this.animationType = animationType;
+  startAnimation(target, animationType = 'slideIn') {
+    if (!target) return;
 
-        if (this.animationType === 'slideIn') {
-            this.#slideIn();
-        } else if (this.animationType === 'slideOut') {
-            return this.#slideOut();
-        }
+    this.target = document.querySelectorAll(target);
+    this.animationType = animationType;
+
+    if (this.animationType === 'slideIn') {
+      this.#slideIn();
     }
-
-    #slideIn() {
-        const totalInd = this.target.length - 1;
-
-        this.target.forEach((el, ind) => {
-            let delay = Number(this.delay * ind);
-            let totalTime = this.time + delay;
-            el.classList.add('hide');
-            setTimeout(() => {
-                el.classList.add('slideIn');
-            }, totalTime);
-
-            // removes the classes required for the animation. This is probably unnecessary but I don't like the extra classes on the elements
-            if (totalInd === ind) {
-                setTimeout(() => {
-                    this.target.forEach((el) => {
-                        el.classList.remove('hide');
-                        el.classList.remove('slideIn');
-                    });
-                }, totalTime + 700);
-            }
-        });
+    if (this.animationType === 'slideOut') {
+      return this.#slideOut();
     }
+  }
 
-    #slideOut() {
-        const totalInd = this.target.length - 1;
-        let endTime = 0;
+  #slideIn() {
+    const totalInd = this.target.length - 1;
 
-        this.target.forEach((el, ind) => {
-            let delay = Number(this.delay * ind);
-            let totalTime = this.time + delay;
+    this.target.forEach((el, ind) => {
+      let delay = Number(this.delay * ind);
+      let totalTime = this.time + delay;
+      el.classList.add('hide');
+      setTimeout(() => {
+        el.classList.add('slideIn');
+      }, totalTime);
 
-            el.classList.add('slideOut');
+      // removes the classes required for the animation. This is probably unnecessary but I don't like the extra classes on the elements
+      if (totalInd === ind) {
+        // console.log('totalInd', totalInd, 'ind', ind);
+        console.log(this.target);
+        setTimeout(() => {
+          this.target.forEach((element) => {
+            // console.log('from Animate', element);
+            console.log('TRUE', element);
 
-            // removes the classes required for the animation. This is probably unnecessary but I don't like the extra classes on the elements
-            if (totalInd === ind) {
-                endTime = totalTime;
-                setTimeout(() => {
-                    this.target.forEach((el) => {
-                        el.classList.remove('slideOut');
-                    });
-                }, totalTime);
-            }
-        });
-        return endTime;
-    }
+            element.classList.remove('hide', 'slideIn');
+            // element.classList.remove('slideIn');
+          });
+        }, totalTime);
+      }
+      console.log(totalTime);
+    });
+  }
+
+  #slideOut() {
+    const totalInd = this.target.length - 1;
+    let endTime = 0;
+
+    this.target.forEach((el, ind) => {
+      let delay = Number(this.delay * ind);
+      let totalTime = this.time + delay;
+
+      el.classList.add('slideOut');
+
+      // removes the classes required for the animation. This is probably unnecessary but I don't like the extra classes on the elements
+      if (totalInd === ind) {
+        // console.log('TRUE');
+        endTime = totalTime;
+        setTimeout(() => {
+          this.target.forEach((element) => {
+            element.classList.remove('slideOut');
+          });
+        }, totalTime);
+      }
+    });
+    return endTime;
+  }
 }
 
 export default new AnimateTransition();
+// export default AnimateTransition();
